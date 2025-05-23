a7a20073433efe252a36b2173976482bca10d36d
nolan freeman abbott <nolan@freemanabbott.com>
2025-05-22 23:06:25 -0500

    typo

--- frontend/src/api/carapi.ts
+++ frontend/src/api/carapi.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const getCars = async (): Promise<CarResponse[]> => {
     const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
 
-    return response.data._embeded.cars;
+    return response.data._embedded.cars;
 }
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
